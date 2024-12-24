782951ae3240208c8f923190d820fe7004548565
Aastha Tanwar <tanwaraastha89@gmail.com>
2024-12-24 23:12:27 +0530

    a

--- src/utlis.py
+++ src/utlis.py
@@ -1,2 +1,23 @@
 # It will include for functionalities that are written in a common way
 # that are used in the entire application.
+
+import os
+import sys
+from src.exception import CustomException
+
+import pandas as pd
+import numpy as np
+import dill
+
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
